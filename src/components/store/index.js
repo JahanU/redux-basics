5c35a75e0111d066f58ff0b361b36ebd3d296c8f
Jahan <34219176+JahanUoL@users.noreply.github.com>
2021-12-25 18:57:09 +0000

    counter and redux store linked via useSelector

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -10,6 +10,7 @@ const reducer = (state = { counter: 0 }, action) => {
             return state
     }
 }
+
 const store = createStore(reducer);
 
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import Store from './components/store/index';
+import store from './components/store/index';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<Provider store={Store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
